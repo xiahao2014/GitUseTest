a236f0889154336036d1c540156564f246e5f505
unknown <xiahao@xiahao.efunor.com>
2016-09-30 17:11:53 +0800

    Initial commit

---
+++ app/src/main/java/speechansj/efunor/com/gitusetest/MainActivity.java
@@ -0,0 +1,13 @@
+package speechansj.efunor.com.gitusetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
