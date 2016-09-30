2fe4597a1eed712439de4d2c1101a97681272d4f
xiahao <xiahaoc@163.com>
2016-09-30 17:53:58 +0800

    新建development

---
+++ fickerprtogress/src/main/java/speechansj/efunor/com/fickerprtogress/NewOperation.java
@@ -0,0 +1,18 @@
+package speechansj.efunor.com.fickerprtogress;
+
+import android.app.Activity;
+
+/**
+ * <br/>Description
+ * <br/>Author: xiahao
+ * <br/>Version 1.0
+ * <br/>Date: 2016-09-30
+ * <br/>Copyright<h3/> Copyright (c) 2016 Shenzhen YiFeng Technology Co., Ltd. Inc.
+ * All rights reserved.
+ */
+public class NewOperation extends Activity{
+    @Override
+    public void setFinishOnTouchOutside(boolean finish) {
+        super.setFinishOnTouchOutside(finish);
+    }
+}
