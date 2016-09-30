58db1182119078a578b619b9aaa752c003476a76
xiahao <xiahaoc@163.com>
2016-09-30 18:11:26 +0800

    在development分支中切换，NewFuncation（新功能分支）

---
+++ fickerprtogress/src/main/java/speechansj/efunor/com/fickerprtogress/NewFuncation.java
@@ -0,0 +1,50 @@
+package speechansj.efunor.com.fickerprtogress;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Master--->Development---->NewFuncation
+ * <br/>Description
+ * <br/>Author: xiahao
+ * <br/>Version 1.0
+ * <br/>Date: 2016-09-30
+ * <br/>Copyright<h3/> Copyright (c) 2016 Shenzhen YiFeng Technology Co., Ltd. Inc.
+ * All rights reserved.
+ */
+public class NewFuncation extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    public NewFuncation() {
+        super();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
